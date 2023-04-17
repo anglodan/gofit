5560b71554eea36e721626ae7da2e510ea021db5
Daniel Barbosa <dan@anglodigital.io>
2023-04-17 18:36:42 -0300

    Worker healthcheck, change port

--- src/index-worker.ts
+++ src/index-worker.ts
@@ -4,6 +4,7 @@ import { config } from './vendure-config';
 
 bootstrapWorker(config)
     .then(worker => worker.startJobQueue())
+    .then(worker => worker.startHealthCheckServer({ port: 80 }))
     .catch(err => {
         console.log(err);
     });
